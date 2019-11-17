69cb35df00b800c9575584c23f9196c1f7f6f094
Mique7827 <55712684+Mique7827@users.noreply.github.com>
2019-11-16 19:20:43 -0600

    Entrega de Scripts de persecución de enemigo.

---
+++ Enemigoper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigoper : Persecucion
+{
+    // Se esta declarando que se va a buscar el punto en el que se encuentre el jugador, mandándola a llamar con el player position.
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("player").GetComponent<Transform>();
+    }
+    // Se esta declarando que se va a “perseguir” la posicion.
+    // Update is called once per frame
+    void Update()
+    {
+        Perseguir(playerPosition, this.gameObject.transform);
+    }
+}
---
+++ Persecucion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persecucion : MonoBehaviour
+{
+    public Transform playerPosition;
+    public float velocidadEnemigo;
+    // Se esta declarando con el public float para poder modificar la velocidad, al igual que la posicion.
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+    // Se esta declarando que se va a “perseguir” la posicion.
+    // Update is called once per frame
+    void Update()
+    {
+        Perseguir(playerPosition, this.gameObject.transform);
+    }
+
+    public void Perseguir(Transform player,Transform enemigo)
+    {
+        Vector3 distancia = player.position - enemigo.position;
+
+        enemigo.LookAt(distancia);
+        enemigo.Translate(distancia.normalized * velocidadEnemigo * Time.deltaTime);
+        // Se esta declarando que seguirá el movimiento del vector que, al mismo tiempo ira asignado al gameobject,Transform.
+
+
+    }
+
+
+}
